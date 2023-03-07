1fdd23f064c01b48fd164c89f6767dae1a1e9be5
Erick Agulló <erickagulopez@outlook.com>
2023-03-07 03:58:27 +0100

    initial

---
+++ vv/src/API.js
@@ -0,0 +1,15 @@
+export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI3M2MxZWYyZC0wZjdhLTQzYzUtYTA4OS1jZjUwZWNiNGU4YTgiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY3ODE1NTY5MSwiZXhwIjoxODM1OTQzNjkxfQ.D87vTzOvUH4zMKnSgUvqjQHncMEZeYxlYaiHiv39p3I";
+// API call to create meeting
+export const createMeeting = async ({ token }) => {
+  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+    method: "POST",
+    headers: {
+      authorization: `${authToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+
+  const { roomId } = await res.json();
+  return roomId;
+};
